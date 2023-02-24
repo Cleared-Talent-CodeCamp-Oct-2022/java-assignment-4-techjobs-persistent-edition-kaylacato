dbe6ea2ea8c8b66da94a67b13906bb12d3c37e20
Kayla Cato <kaylamwalker95@gmail.com>
2023-02-24 12:51:47 -0500

    All Done!

--- queries.sql
+++ queries.sql
@@ -1,19 +1,19 @@
 -- Part 1: Test it with SQL
--- id INT,
--- employer VARCHAR(255),
--- name VARCHAR(255),
--- skills VARCHAR(255)
+ id INT,
+ employer VARCHAR(255),
+ name VARCHAR(255),
+ skills VARCHAR(255)
 
 -- Part 2: Test it with SQL
---  SELECT name
---  FROM employer
---  WHERE location='St.Louis, MO';
+  SELECT name
+  FROM employer
+  WHERE location='St.Louis, MO';
 
 -- Part 3: Test it with SQL
---  DROP TABLE job;
+  DROP TABLE job;
 
 -- Part 4: Test it with SQL
---  SELECT name
---  FROM skill
---  INNER JOIN job_skills ON skill.id = job_skills.skills_id
---  ORDER BY skill.name;
+  SELECT name
+  FROM skill
+  INNER JOIN job_skills ON skill.id = job_skills.skills_id
+  ORDER BY skill.name ASC;
